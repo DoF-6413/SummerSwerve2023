c8874980a9a3b3fa054a746cb77670321686c928
nancyesquivel10 <nancyesquivel505@gmail.com>
2023-05-27 14:23:58 -0700

    wip implemented modules together needs testing

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -1,32 +1,37 @@
 package frc.robot.subsystems.drive;
 
+import java.util.Arrays;
+
 import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.subsystems.gyro.Gyro;
 import frc.robot.subsystems.gyro.GyroIO;
 import frc.robot.subsystems.gyro.GyroIO.GyroIOInputs;
 
 public class Drive extends SubsystemBase {
 
-  private final GyroIO gyroIO;
-  private final GyroIOInputs gyroIOInputs = new GyroIOInputs();
   private final ModuleIOInputsAutoLogged inputs = new ModuleIOInputsAutoLogged();
   private static final Module[] modules = new Module[4];
+  private final Gyro gyro;
 
   private double maxAngularSpeed;
-  private SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics();
+  private SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(getModuleTranslations());
 
   private ChassisSpeeds setpoint = new ChassisSpeeds();
   private SwerveModuleState[] lastSetpointStates = new SwerveModuleState[] {
@@ -45,11 +50,10 @@ public class Drive extends SubsystemBase {
   private Twist2d fieldVelocity = new Twist2d();
 
   /** Creates a new Drive. */
-  public Drive(GyroIO gyroIO, moduleIO flModuleIO, moduleIO frModuleIO, moduleIO blModuleIO, moduleIO brModuleIO) {
-
-    this.gyroIO = gyroIO;
+  public Drive(moduleIO flModuleIO, moduleIO frModuleIO, moduleIO blModuleIO, moduleIO brModuleIO, Gyro gyro) {
 
     System.out.println("[Init] Creating Drive");
+    this.gyro = gyro;
 
     modules[0] = new Module(flModuleIO, 0);
     modules[1] = new Module(frModuleIO, 1);
@@ -64,18 +68,94 @@ public class Drive extends SubsystemBase {
   @Override
   public void periodic() {
 
-    gyroIO.updateInputs(gyroIOInputs);
-    Logger.getInstance().processInputs("Drive/Gyro", gyroIOInputs);
     for (var module : modules) {
       module.periodic();
     }
 
     Logger.getInstance().processInputs("Drive", inputs);
+    swerveKinematics = new SwerveDriveKinematics(getModuleTranslations());
+    maxAngularSpeed = DrivetrainConstants.maxLinearSpeed
+        / Arrays.stream(getModuleTranslations())
+            .map(translation -> translation.getNorm())
+            .max(Double::compare)
+            .get();
 
-    // Update odometry and log the new pose
-    odometry.update(new Rotation2d(-Math.toDegrees(gyroIOInputs.yawPositionRad)), getLeftPositionMeters(),
-        getRightPositionMeters());
-    Logger.getInstance().recordOutput("Odometry", getPose());
+  // Run modules
+  if (DriverStation.isDisabled()) {
+    // Stop moving while disabled
+    for (var module : modules) {
+      module.stop();
+    }
+  }
+
+    //Clears the setpoint logs 
+    Logger.getInstance().recordOutput("SwerveStates/Setpoints", new double[] {});
+    Logger.getInstance().recordOutput("SwerveStates/SetpointsOptimized", new double[] {});
+   
+          // Calculate module setpoints
+          var setpointTwist =
+          new Pose2d()
+              .log(
+                  new Pose2d(
+                      setpoint.vxMetersPerSecond * DrivetrainConstants.loopPeriodSecs,
+                      setpoint.vyMetersPerSecond * DrivetrainConstants.loopPeriodSecs,
+                      new Rotation2d(setpoint.omegaRadiansPerSecond * DrivetrainConstants.loopPeriodSecs)));
+      var adjustedSpeeds =
+          new ChassisSpeeds(
+              setpointTwist.dx / DrivetrainConstants.loopPeriodSecs,
+              setpointTwist.dy / DrivetrainConstants.loopPeriodSecs,
+              setpointTwist.dtheta / DrivetrainConstants.loopPeriodSecs);
+      SwerveModuleState[] setpointStates = swerveKinematics.toSwerveModuleStates(adjustedSpeeds);
+      SwerveDriveKinematics.desaturateWheelSpeeds(setpointStates, DrivetrainConstants.maxLinearSpeed);
+
+      // Send setpoints to modules
+      SwerveModuleState[] optimizedStates = new SwerveModuleState[4];
+      for (int i = 0; i < 4; i++) {
+        optimizedStates[i] = modules[i].runSetpoint(setpointStates[i]);
+      }
+
+      // Log setpoint states
+      Logger.getInstance().recordOutput("SwerveStates/Setpoints", setpointStates);
+      Logger.getInstance().recordOutput("SwerveStates/SetpointsOptimized", optimizedStates);
+
+          // Log measured states
+    SwerveModuleState[] measuredStates = new SwerveModuleState[4];
+    for (int i = 0; i < 4; i++) {
+      measuredStates[i] = modules[i].getState();
+    }
+    Logger.getInstance().recordOutput("SwerveStates/Measured", measuredStates);
+
+    // Update odometry
+    SwerveModulePosition[] wheelDeltas = new SwerveModulePosition[4];
+    for (int i = 0; i < 4; i++) {
+      wheelDeltas[i] =
+          new SwerveModulePosition(
+              (modules[i].getPositionMeters() - lastModulePositionsMeters[i]),
+              modules[i].getAngle());
+      lastModulePositionsMeters[i] = modules[i].getPositionMeters();
+    }
+
+    var twist = swerveKinematics.toTwist2d(wheelDeltas);
+    var gyroYaw = new Rotation2d(gyro.getYaw().getRadians());
+    if (gyro.isConnected()) {
+      twist = new Twist2d(twist.dx, twist.dy, gyroYaw.minus(lastGyroYaw).getRadians());
+    }
+    lastGyroYaw = gyroYaw;
+    // poseEstimator.addDriveData(Timer.getFPGATimestamp(), twist);
+    // Logger.getInstance().recordOutput("Odometry/Robot", getPose());
+
+    // Update field velocity
+    ChassisSpeeds chassisSpeeds = swerveKinematics.toChassisSpeeds(measuredStates);
+    Translation2d linearFieldVelocity =
+        new Translation2d(chassisSpeeds.vxMetersPerSecond, chassisSpeeds.vyMetersPerSecond)
+            .rotateBy(gyro.getYaw());  // TODO: change this to pose estimator get rotation 
+    fieldVelocity =
+        new Twist2d(
+            linearFieldVelocity.getX(),
+            linearFieldVelocity.getY(),
+            gyro.isConnected()
+                ? gyro.getYawVelocity()
+                : chassisSpeeds.omegaRadiansPerSecond);
   }
 
   // TODO:continue the periodic
@@ -108,21 +188,29 @@ public class Drive extends SubsystemBase {
           getModuleTranslations()[i].getAngle());
     }
   }
+
   // returns max linear speeds in terms of meters per sec
   public double getMaxLinearSpeedMetersPerSec() {
     return DrivetrainConstants.maxLinearSpeed;
   }
 
-  // returns max angular speed in radians per second 
+  // returns max angular speed in radians per second
   public double getMaxAngularSpeedRadPerSec() {
     return maxAngularSpeed;
   }
 
-  public Twist2d
+  public Twist2d getFieldVelocity() {
+    return fieldVelocity;
+  }
 
-  /** Returns the current odometry pose in meters. */
-  public Pose2d getPose() {
-    return odometry.getPoseMeters();
+  /** Returns an array of module translations. */
+  public Translation2d[] getModuleTranslations() {
+    return new Translation2d[] {
+        new Translation2d(DrivetrainConstants.trackWidthX / 2.0, DrivetrainConstants.trackWidthY / 2.0),
+        new Translation2d(DrivetrainConstants.trackWidthX / 2.0, -DrivetrainConstants.trackWidthY / 2.0),
+        new Translation2d(-DrivetrainConstants.trackWidthX / 2.0, DrivetrainConstants.trackWidthY / 2.0),
+        new Translation2d(-DrivetrainConstants.trackWidthX / 2.0, -DrivetrainConstants.trackWidthY / 2.0)
+    };
   }
 
 }
---
+++ src/main/java/frc/robot/subsystems/gyro/Gyro.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.gyro;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Add your docs here. */
+public class Gyro extends SubsystemBase {
+    public static GyroIO gyroIO;
+    public static GyroIOInputsAutoLogged gyroInputs = new GyroIOInputsAutoLogged();
+
+    public Gyro(GyroIO io) {
+        gyroIO = io;
+    }
+
+    public void periodic() {
+        gyroIO.updateInputs(gyroInputs);
+        Logger.getInstance().processInputs("Gyro", gyroInputs);
+    }
+
+    /** Returns the current pitch (Y rotation). */
+    public Rotation2d getPitch() {
+        return new Rotation2d(gyroInputs.pitchPositionRad);
+    }
+
+    /** Returns the current roll (X rotation). */
+    public Rotation2d getRoll() {
+        return new Rotation2d(gyroInputs.rollPositionRad);
+    }
+
+    public Rotation2d getYaw() {
+        return new Rotation2d(gyroInputs.yawPositionRad);
+    }
+
+    /** Returns the current yaw velocity (Z rotation) in radians per second. */
+    public double getYawVelocity() {
+        return gyroInputs.yawVelocityRadPerSec;
+    }
+
+    /** Returns the current pitch velocity (Y rotation) in radians per second. */
+    public double getPitchVelocity() {
+        return gyroInputs.pitchVelocityRadPerSec;
+    }
+
+    /** Returns the current roll velocity (X rotation) in radians per second. */
+    public double getRollVelocity() {
+        return gyroInputs.rollVelocityRadPerSec;
+    }
+
+    public boolean isConnected() {
+        return gyroInputs.connected;
+    }
+}
