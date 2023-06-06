14d4a470473709d5c140b03378103bf641faba33
victor oundjian <vgo89ed@gmail.com>
2023-06-05 19:49:39 -0700

    WIP pose esitmator

--- src/main/java/frc/robot/subsystems/pose/Pose.java
+++ src/main/java/frc/robot/subsystems/pose/Pose.java
@@ -8,10 +8,13 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardInput;
 import org.photonvision.PhotonPoseEstimator;
 
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.Mode;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.gyro.Gyro;
 import frc.robot.subsystems.vision.Vision;
@@ -22,7 +25,7 @@ private Field2d field2d;
 private Drive drive;
 private Gyro gyro;
 private Vision vision;
-private PhotonPoseEstimator p 
+private SwerveDrivePoseEstimator poseEstimator; 
     public Pose(Drive drive, Gyro gyro, Vision vision, SwerveDriveKinematics kinematics) {
         field2d = new Field2d();
         SmartDashboard.putData(field2d);
@@ -30,7 +33,30 @@ private PhotonPoseEstimator p
         this.gyro = gyro;
         this.vision = vision;
 
-        poseEstimator = new SwerveDrivePoseEstimator(kinematics, this.gyro.getYaw(), this.drive.getSwerveModulePositions(), )
+        poseEstimator = new SwerveDrivePoseEstimator(kinematics, this.gyro.getYaw(), this.drive.getSwerveModulePositions(), new Pose2d());
         
+    } 
+
+    @Override 
+    public void periodic() {
+
+        if()
+    }
+
+
+    public Pose2d getCurrentPose2d() {
+        return poseEstimator.getEstimatedPosition();
+    }
+
+    public void resetPose(Pose2d currentPose2d) {
+        poseEstimator.resetPosition(gyro.getYaw(), drive.getSwerveModulePositions(), currentPose2d);
+    }
+
+    public void setPose2d() {
+        if(Constants.getMode() == Mode.SIM) {
+            setPose()
+        }
+
+    
     }
 }
