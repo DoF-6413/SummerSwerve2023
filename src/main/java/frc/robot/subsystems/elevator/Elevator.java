773a53f15483961d3b0339d75f2d267e1c2071ea
El1zabethS <Bestlemon555@gmail.com>
2023-08-01 20:00:02 -0700

    defined basic functions

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -5,4 +5,28 @@
 package frc.robot.subsystems.elevator;
 
 /** Add your docs here. */
-public class Elevator {}
+public class Elevator {
+
+    public boolean limitSwitchPressed(){
+        return false;
+    }
+
+    public double getElevatorPositionMeters(){
+        return 0.0;
+    } 
+    
+    public double getElevatorHeightMeters(){
+        return 0.0;
+    }
+    
+    public double elevatorVelocityMetersPerSec(){
+        return 0.0;
+    }
+
+    public double elevatorPositionFeet(){
+        return 0.0;
+    }    
+
+    public void setElevatorVoltageOutput(){
+    }
+}
