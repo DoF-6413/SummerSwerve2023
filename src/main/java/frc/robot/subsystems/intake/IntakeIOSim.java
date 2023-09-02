29c1f845fc78798f6c4e267a27978006e4efa24d
RAHAKHAN2008@gmail.com <sawyerlee29@gmail.com>
2023-09-02 13:41:16 -0700

    finished intake subsystem, needs testing

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -9,18 +9,25 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import frc.robot.Constants;
 import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.IntakeConstants.IntakeMotor;
 
 /** Add your docs here. */
 public class IntakeIOSim implements IntakeIO {
-    private FlywheelSim intakeSim = new FlywheelSim(DCMotor.getNEO(2), IntakeConstants.gearRatio, 0); //TODO: what is jKgMetersSquared?????
+    private FlywheelSim intakeMotorSim = new FlywheelSim(DCMotor.getNEO(2), IntakeConstants.gearRatio, 0); //TODO: what is jKgMetersSquared?????
     
-    private double intakeAppliedVolts = 0.0;
+    private double intakeAppliedVolts = 0.0; //TODO find volts ??
     public IntakeIOSim() {
-
     }
 
     public void updateInputs(IntakeIOInputs inputs) {
-        inputs.intakePositionRad += intakeSim.getAngularVelocityRadPerSec() * Constants.loopPeriodSecs;
+        inputs.intakePositionRad += intakeMotorSim.getAngularVelocityRadPerSec() * Constants.loopPeriodSecs;
+        inputs.intakeVelocityRadPerSec = intakeMotorSim.getAngularVelocityRadPerSec();
+        inputs.intakeAppliedVolts = 0.0;
+        inputs.intakeCurrentAmps = new double[] {Math.abs(intakeMotorSim.getCurrentDrawAmps())};
+        inputs.intakeTempCelcius = new double[] {};        
+    }
 
+    public void setPercentSpeed(double percent) {
+        intakeMotorSim.setInputVoltage(percent * intakeAppliedVolts);
     }
 }
