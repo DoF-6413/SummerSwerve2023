2d1e0f969fc1d7586d7c8d51d01011f8d2bef80d
alt09 <andreslaratrevino@gmail.com>
2023-09-20 19:50:20 -0700

    change flywheel to singlejointedarmsim WIP not tested

--- src/main/java/frc/robot/subsystems/wrist/WristIOSim.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIOSim.java
@@ -6,14 +6,14 @@ package frc.robot.subsystems.wrist;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import frc.robot.Constants;
 
 import frc.robot.Constants.WristNeoConstants;
 
 public class WristIOSim implements WristIO {
-    private FlywheelSim WristMotor = new FlywheelSim(DCMotor.getNEO(1),WristNeoConstants.gearRatio,1);
-
+    private SingleJointedArmSim WristMotor = new SingleJointedArmSim(DCMotor.getNEO(1),WristNeoConstants.gearRatio, 1,0.2, 0.5, 1, true);//TODO: update this values 
+//DCMotor.getNEO(1),WristNeoConstants.gearRatio,1
     public WristIOSim(){
          System.out.println("[Init] Creating WristIOSim");
          
@@ -22,8 +22,8 @@ public class WristIOSim implements WristIO {
     @Override
     public void updateInputs(WristIOInputs inputs) {
         WristMotor.update(Constants.loopPeriodSecs);
-        inputs.turnPositionRad += WristMotor.getAngularVelocityRadPerSec() * Constants.loopPeriodSecs;
-        inputs.turnVelocityRadPerSec = WristMotor.getAngularVelocityRadPerSec();
+        inputs.turnPositionRad += WristMotor.getVelocityRadPerSec() * Constants.loopPeriodSecs;
+        inputs.turnVelocityRadPerSec = WristMotor.getVelocityRadPerSec();
         inputs.turnAppliedVolts = 0.0;
         inputs.turnCurrentAmps = Math.abs(WristMotor.getCurrentDrawAmps()); 
         inputs.WristTempCelcius = 0.0;
