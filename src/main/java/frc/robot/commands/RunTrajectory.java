adad8086adfb2fccdb3efadc588782a5e0bc4a21
DragonHype <josueraygoza2@gmail.com>
2023-10-16 19:06:25 -0700

    Adjusted path1 and PID values in RunTrajectory, doesn't seem to run accurately in sim

--- src/main/java/frc/robot/commands/RunTrajectory.java
+++ src/main/java/frc/robot/commands/RunTrajectory.java
@@ -38,9 +38,9 @@ public class RunTrajectory extends SequentialCommandGroup {
       new PPSwerveControllerCommand(
         traj, 
         pose::getCurrentPose2d, 
-        new PIDController(0, 0.1, 0), 
-        new PIDController(0, 0.1 ,0), 
-        new PIDController(0, 0,0), 
+        new PIDController(0.5, 0.0, 0.0), 
+        new PIDController(0.0, 0.0, 0.0), 
+        new PIDController(0.0, 0.0, 0.0), 
         drive::runVelocity,  
         true,
         drive,
